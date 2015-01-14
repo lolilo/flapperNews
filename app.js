8f567245ad07ca15adcff9b2e3b01ee0c332842f
Liana Lo <liana.lo@prezi.com>
2015-01-15 00:38:29 +0100

    enable upvote on click

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ angular.module('flapperNews', [])
       ];
       $scope.addPost = function() {
         if (!$scope.title || $scope.title === '') { return; }
-        $scope.posts.push({title: $scope.title, updvotes: 0});
+        $scope.posts.push({title: $scope.title, upvotes: 0});
         $scope.title = '';
+      };
+      $scope.incrementUpvotes = function(post) {
+        post.upvotes +=1;
       };
 }]);
